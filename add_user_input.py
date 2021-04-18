a189027946bc70cb64a3aa80038d71ad53f3da9f
oggy-crypto <82752169+oggy-crypto@users.noreply.github.com>
2021-04-18 14:08:53 +0530

    Add files via upload

---
+++ add_user_input.py
@@ -0,0 +1,15 @@
+number_1=int( input("first number: "))
+number_2= int(input("\nsecond number : "))
+
+sum1=number_1/number_2
+sum2=number_1+number_2
+sum3=number_1-number_2
+sum4=number_1*number_2
+#print(sum1,sum2,sum3,sum4)
+print("additiion-> ",sum2)
+print("division->",sum1)
+print("subtraction->",sum3)
+print("multiplication->",sum4)
+
+
+
---
+++ addition_of_two_numbers.py
@@ -0,0 +1,9 @@
+num1=15
+num2=3
+sum=num1+num2
+print(sum)
+
+numa=16
+numb=4
+sum=numa+numb
+print(sum)
